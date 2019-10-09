83924c5bfc94eb30928d86d5bb776c01e7942caf
syedsharjil <syedsharjil@outlook.com>
2019-10-10 01:50:18 +0530

    day 3 added

---
+++ day3.py
@@ -0,0 +1,23 @@
+import math
+import os
+import random
+import re
+import sys
+def weird(N1):
+    # function to check all the given cases
+    if(N1%2!=0):
+        print("Weird")
+    else:
+        if(N1>=2 and N1<=5):
+            print("Not Weird")
+        if(N1>=6 and N1<=20):
+            print("Weird")
+        if(N1>20):
+            print("Not Weird")
+
+
+
+
+if __name__ == '__main__':
+    N = int(raw_input())
+    weird(N)
